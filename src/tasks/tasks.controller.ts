ad58c7a536b4ebd40b431478e4349e29447bf053
Jay Tailor <tailorj64@gmail.com>
2023-08-01 13:40:35 +0530

    Feat: Get all tasks endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,5 +6,10 @@ export class TasksController {
     constructor(
         private tasksService: TasksService,
     ) {}
+
+    @Get()
+    getAllTasks() {
+        return this.tasksService.getAllTasks();
+    }
 
 }
