03d675470244509603234e92f8f0a14c6c159cfe
Jay Tailor <tailorj64@gmail.com>
2023-08-02 17:39:59 +0530

    Feat: Add auth guard for all task endpoints

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { Task } from './task.entity';
 import { TasksController } from './tasks.controller';
 import { TaskRepository } from './tasks.repository';
@@ -8,6 +9,7 @@ import { TasksService } from './tasks.service';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Task]),
+    AuthModule,
   ],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository]
